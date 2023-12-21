b3a83189da173da807130e83ae48d41bd991918d
S3ccles <149751441+S3ccles@users.noreply.github.com>
2023-12-21 18:16:42 -0500

    linking

--- JS/Control Flow/growingpains.js
+++ JS/Control Flow/growingpains.js
@@ -3,4 +3,21 @@ const radius= 5;
 const area= PI * radius * radius;
 const minPlantSpace= 0.8;
 const intialplantcount= 20;
-const weeklyGrowthRate
+const weeklyGrowthRate= 2;
+
+// Calculate plant growth
+function predictPlantGrowthAndAction(weeks:any):any 
+let currentPlantCount = intialplantcount;
+for (let i = 0; i < weeks; i++) {currentPlantCount=currentPlantCount * weeklyGrowthRate;
+const maximumPlantCapacity = Math.floor(area / minimumPlantSpace);
+  const eightyPercentLimit = Math.floor(maximumPlantCapacity * 0.8);
+  const fiftyPercentLimit = Math.floor(maximumPlantCapacity * 0.5);
+if (plantCount > eightyPercentLimit) {
+    console.log('Action: Prune plants');
+  } else if (plantCount > fiftyPercentLimit) {
+    console.log('Action: Monitor plant growth');
+  } else {
+    console.log('Action: Plant more seedlings');
+  }
+}
+
